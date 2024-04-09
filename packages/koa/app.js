c4d9d621ffaa2962c29c56f2d0ce6b4dc3d22159
dodo <rguo9659@gmail.com>
2024-04-09 22:59:52 +0800

    static prefix

--- packages/koa/app.js
+++ packages/koa/app.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
 const app = new Koa();
 const serve = require('koa-static')
+const mount = require('koa-mount')
 
-app.use(serve('.'))
+// 给静态资源加前缀
+app.use(mount('/static',serve('.')))
 
 // http://localhost:3000/package.json
 app.listen(3000,()=>{
