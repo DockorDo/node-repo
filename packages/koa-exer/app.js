1d64e730b54c2a52489b826faba183fe84aeaf84
dodo <rguo9659@gmail.com>
2024-04-10 22:53:29 +0800

    file upload

--- packages/koa-exer/app.js
+++ packages/koa-exer/app.js
@@ -1,9 +1,29 @@
 const Koa = require("koa");
 const app = new Koa();
-const bodyparser = require("koa-bodyparser")();
+const bodyparser = require("koa-bodyparser");
 const router = require("koa-router")();
+const multer = require('koa-multer')
+const path = require('path')
 
-app.use(bodyparser);
+// 文件上传配置
+const storage = multer.diskStorage({
+  destination: (req,file,cb)=>{
+    cb(null,path.join(__dirname,'/uploads'))
+  },
+  filename:(req,file,cb)=>{
+    let type = file.originalname.split('.')[1]
+    cb(null, `${file.fieldname}-${Date.now().toString(16)}.${type}`)
+  }
+})
+const limits = {
+  files:1
+}
+
+const upload = multer({dest:'uploads/',storage,limits})
+
+app.use(bodyparser({
+  enableTypes:['json','form']
+}));
 
 router.post("/post", (ctx) => {
   ctx.body = ctx.request.body;
@@ -12,6 +32,21 @@ router.post("/post", (ctx) => {
 router.get("/get", (ctx) => {
   ctx.body = ctx.query
 });
+
+router.post('/post/formdata', upload.any(),ctx=>{
+  console.log(ctx.req.files);
+  ctx.body={
+    status:{
+      code:0,
+      msg:'upload success'
+    },
+    data:{
+      body:ctx.req.body,
+      files:ctx.req.files
+    }
+  }
+})
+
 
 app.use(router.routes(), router.allowedMethods());
 
