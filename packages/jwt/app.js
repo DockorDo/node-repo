fcc72c1c0e9d99e565a61fa5c2a894f5e82cb366
dodo <rguo9659@gmail.com>
2024-04-13 00:28:30 +0800

    get jwt

--- packages/jwt/app.js
+++ packages/jwt/app.js
@@ -3,6 +3,12 @@ const app = new koa()
 const router = require("koa-router")()
 const jwt = require('koa-jwt')
 
+/*
+ jwt可以通过三种方式传回
+ 1. ctx.request.body.token
+ 2. ctx.query.token
+ 3. ctx.headers['x-access-token']
+*/
 router.get('/api',jwt({secret:'shared-secret'}),(ctx,next)=>{
     ctx.body={msg:'ok'}
 })
